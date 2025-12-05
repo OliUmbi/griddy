0b1c0bd3dd6c8d9d2bc2d88b37dc606af33a971d
Shotkick <96243168+Shotkick@users.noreply.github.com>
2025-12-05 13:44:58 +0100

    Changed Connection.java from interface to abstract class

--- src/main/java/su/fhso/ln1/model/connections/Connection.java
+++ src/main/java/su/fhso/ln1/model/connections/Connection.java
@@ -1,6 +1,16 @@
 package su.fhso.ln1.model.connections;
 
-public interface Connection {
+public abstract class Connection {
 
-    boolean connects(Orientation orientation);
+    private int rotation;
+
+    public int getRotation() {
+        return rotation;
+    }
+
+    public void setRotation(int rotation) {
+        this.rotation = rotation;
+    }
+
+    public abstract boolean connects(Orientation orientation);
 }
